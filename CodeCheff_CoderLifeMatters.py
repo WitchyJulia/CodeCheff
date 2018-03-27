5da593687f1288478d68c76e891f908798133b75
WitchyJulia <37779716+WitchyJulia@users.noreply.github.com>
2018-03-27 21:02:54 +0200

    Add files via upload

---
+++ CodeCheff_CoderLifeMatters.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+
+# In[47]:
+
+
+days = []
+share = raw_input("Numbers:").split()
+for word in share:
+    days.append(word)
+test_cases = days[:1:]
+for number in test_cases:
+    cases = int(number)
+count = 31
+startup = 2
+
+for number in range(0,cases):
+    start = int(startup)
+    line = days[start:count:1]
+    str_lines = str(line)
+    str_line = "".join(line)
+    happines = "111111"
+        
+    if happines in str_line:
+        print "#coderlifematters"
+    else:
+        print "#allcodersarefun"
+    startup = int(startup + 30)
+    count = int(count+30)
+
+
