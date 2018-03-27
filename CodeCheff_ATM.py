f50f69718423ae9f454625b18210c4f7c724f98d
WitchyJulia <37779716+WitchyJulia@users.noreply.github.com>
2018-03-27 06:52:22 +0200

    Add files via upload

---
+++ CodeCheff_ATM.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+input_ = raw_input()
+a,b = input_.split(" ")[:2]
+a = float(a)
+b = float(b)
+if a%5 == 0 and b > a + 0.5:
+	b = b - a
+	b = b - 0.50
+print("%.2f" % b)
+
